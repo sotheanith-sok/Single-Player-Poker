734301adee2d95dd73cb2edc0d60d16d42b39470
sotheanith <thetheanith@gmail.com>
2015-12-05 15:01:38 -0800

    Add variables

--- CECS_174_Final_Project/src/Poker_Main.java
+++ CECS_174_Final_Project/src/Poker_Main.java
@@ -1,9 +1,18 @@
+import java.util.*;
+
 
 public class Poker_Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//Variable control;
+		double bet; //The amount player bet
+		double payout; //The amount player payout
+		double total; //Total of money player has.
+		boolean checker; //Check input
+		ArrayList<Integer> index=new ArrayList<Integer>(); //This array will be used to save index chosen by player of the card to be remove. 
+		
+		
 	}
 
 }
--- CECS_174_Final_Project/src/Poker_Method.java
+++ CECS_174_Final_Project/src/Poker_Method.java
@@ -1,4 +1,16 @@
+/**This class will contain all the necessary  methods required by the class "Poker_Main."
+ * 
+ * @author:
+ *
+ */
 
+import java.util.*;
 public class Poker_Method {
+	//Variable Control
+	private ArrayList<String> deck=new ArrayList<String>(); // The shuffled deck will store in here
+	private ArrayList<String> player_hand=new ArrayList<String>();// Cards that player has
+	private ArrayList<String> discard=new ArrayList<String>(); //Discarded card will save into here
+	
+	
 
 }
